cac2d4d6965420893a712d86de603de22a7b429b
22FI117YAMAGUCHI <166776713+22FI117YAMAGUCHI@users.noreply.github.com>
2024-06-14 12:16:48 +0900

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "山田先生"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
